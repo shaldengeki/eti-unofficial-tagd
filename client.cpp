f0ed848e0a38211854cb6bce81542f6ea35e2ea2
Charles Guo <shaldengeki@gmail.com>
2014-02-16 23:09:19 -0600

    client: can specify socket address

--- client.cpp
+++ client.cpp
@@ -8,50 +8,69 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+#include <boost/program_options.hpp>
+namespace options = boost::program_options;
+
 int main(void)
 {
-    int s, t, len;
-    struct sockaddr_un remote;
-    char* message = new char[500];
+  options::options_description allOptions("Options");
 
-    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
+  allOptions.add_options()
+    ("help", "produce this help message")
+    ("socket", options::value<std::string>()->default_value("/tmp/tagd.sock"), "path to socket")
+  ;
+  options::variables_map vm;
+  options::store(options::parse_command_line(argc, argv, allOptions), vm);
+  options::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << allOptions << std::endl;
+    return 1;
+  }
+
+
+  int s, t, len;
+  struct sockaddr_un remote;
+  char* message = new char[500];
+
+  if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  printf("Trying to connect...\n");
+
+  remote.sun_family = AF_UNIX;
+
+  strcpy(remote.sun_path, vm["socket"].as<std::string>().c_str());
+  len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+  if (connect(s, (struct sockaddr *)&remote, len) == -1) {
+    perror("connect");
+    exit(1);
+  }
+
+  printf("Connected.\n");
+
+  while(printf("> "), fgets(message, 500, stdin), !feof(stdin)) {
+    if (send(s, message, strlen(message), 0) == -1) {
+      perror("send");
+      exit(1);
     }
 
-    printf("Trying to connect...\n");
-
-    remote.sun_family = AF_UNIX;
-
-    strcpy(remote.sun_path, "/home/shaldengeki/tagd.sock");
-    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
-    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
-        perror("connect");
-        exit(1);
+    if ((t=recv(s, message, 500, 0)) > 0) {
+      unsigned int* int_buffer = reinterpret_cast<unsigned int*>(message);
+      for (int i = 0; i < 50; ++i) {
+        std::cout << int_buffer[i] << std::endl;
+      }
+    } else {
+      if (t < 0) perror("recv");
+      else printf("Server closed connection\n");
+      exit(1);
     }
+  }
 
-    printf("Connected.\n");
+  delete message;
+  close(s);
 
-    while(printf("> "), fgets(message, 500, stdin), !feof(stdin)) {
-        if (send(s, message, strlen(message), 0) == -1) {
-            perror("send");
-            exit(1);
-        }
-
-        if ((t=recv(s, message, 500, 0)) > 0) {
-            unsigned int* int_buffer = reinterpret_cast<unsigned int*>(message);
-            for (int i = 0; i < 50; ++i) {
-                std::cout << int_buffer[i] << std::endl;
-            }
-        } else {
-            if (t < 0) perror("recv");
-            else printf("Server closed connection\n");
-            exit(1);
-        }
-    }
-
-    delete message;
-    close(s);
-
-    return 0;
+  return 0;
 }
